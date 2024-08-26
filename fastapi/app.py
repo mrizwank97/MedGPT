81b5f5ef7a7f978744dfd7d18e7233ad998b6e4d
Muhammad Rizwan Khalid <rizwan@MacBook-Air-8.local>
2024-08-26 02:45:19 +0200

    add linting

--- fastapi/app.py
+++ fastapi/app.py
@@ -4,28 +4,31 @@ from fastapi import FastAPI, Response
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return {"hello" : "World"}
 
-@app.get('/ask')
-def ask(prompt :str, seed: Union[int, None] = None, max_tokens: Union[int, None] = None):
-    
-    #check if seed is provided
+@app.get("/")
+def home():
+    return {"hello": "World"}
+
+
+@app.get("/ask")
+def ask(
+    prompt: str, seed: Union[int, None] = None, max_tokens: Union[int, None] = None
+):
     options = {}
     if seed is not None:
-        options["seed"]=seed
-        
-    if max_tokens is not None:
-        options["num_predict"]=max_tokens
-    
-    res = requests.post('http://ollama:11434/api/generate', json={
-        "prompt": prompt,
-        "stream" : False,
-        "model" : "qwen2:0.5b",
-        "options" : options
-    })
-    # add options to the request
+        options["seed"] = seed
 
+    if max_tokens is not None:
+        options["num_predict"] = max_tokens
+
+    res = requests.post(
+        "http://ollama:11434/api/generate",
+        json={
+            "prompt": prompt,
+            "stream": False,
+            "model": "qwen2:0.5b",
+            "options": options,
+        },
+    )
 
     return Response(content=res.text, media_type="application/json")
--- fastapi/test.py
+++ fastapi/test.py
@@ -2,12 +2,13 @@ import requests
 from fastapi.testclient import TestClient
 from app import app
 
+client = TestClient(app)
+
+
 def test_ask():
-    
-    client = TestClient(app)
     prompt = "What is capital of France?"
     expected_response = "French capital is Paris."
-    response = client.get('/ask', params={"prompt": prompt, "seed": 123})
-    assert response.json()['response'] == expected_response
+    response = client.get("/ask", params={"prompt": prompt, "seed": 123})
+
+    assert response.json()["response"] == expected_response
     assert response.status_code == 200
-    
